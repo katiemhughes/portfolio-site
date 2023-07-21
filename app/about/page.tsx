8ffa6b84e8912490ad8bf2198ec4eb50e264ac25
katiemhughes <katie.m.hughes@hotmail.co.uk>
2023-07-21 18:19:15 +0100

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,17 @@
+import './about.scss';
+
 export default function About() {
   return (
-    <main>
-        <h1>About</h1>
-        <section>
-          This will be a section all about me.
+    <main className='about'>
+        <h1 className='about__title'>About Me<span className='about__title--lilac'>.</span></h1>
+        <section className='about__section'>
+          <p>I&apos;m Katie, a junior developer who is currently working in Optimisation and A/B testing.</p>
+          <p>At the beginning of the COVID-19 lockdown, I lost my job. I&apos;d been doing temporary office work for a few years because I didnt know what I wanted to do after uni (I had studied Spanish and French, so I wasn&apos;t left with a very clear career path). So, instead of letting this get me down, I decided that this was the perfect opportunity for a career change.</p>
+          <p>I had recently taken up music production as a hobby, live coding music on a program called Sonic Pi. So, I decided to put this skill to good use. I did some FreeCodeCamp courses and followed some YouTube tutorials on how to make websites using HTML, Javascript and CSS, and then landed a scholarship for a bootcamp. The scholarship was awarded by an organisation called Digital Her, and the bootcamp was the 3-month Develop course at Code Nation.</p>
+          <p>After completing the bootcamp, I then obtained an apprenticeship following a year long placement. I then finally landed my job at Eclipse Group Solutions, where I currently work as a Junior Optimisation Developer.</p>
+          <p>I do find A/B testing interesting, particularly because it&apos;s interesting to monitor users behaviour while browsing a website. However, I&apos;m now ready for a change, and I&apos;m actively looking for a role where I can use React and Typescript, rather than solely working with vanilla Javascript, which is what I&apos;m currently doing.</p>
+          <p>I am building this website using Next.js, React and Typescript, and I&apos;m also working on a larger scale project which is a website for my DJ collective. I&apos;m building that using React hooked up to a headless CMS called Contentful, so that the other members of my collective are able to update the website. If you&apos;d like to take a look at this site, you can take a peep on my Projects page.</p>
+          <p>By building both of these websites in tandem, I&apos;m confident that I will be able to upskill and learn everything that I need to know in order to walk into a new, React-based role.</p>
         </section>
     </main>
 )
