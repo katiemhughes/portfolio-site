f63dd85868b06fc241a6eee80fc87a2803b738e9
katiemhughes <katie.m.hughes@hotmail.co.uk>
2023-09-20 23:07:10 +0100

    style change

--- app/components/skeleton-post/SkeletonPost.tsx
+++ app/components/skeleton-post/SkeletonPost.tsx
@@ -2,12 +2,12 @@ import './skeleton-post.scss';
 
 export default function SkeletonPost() {
   return (
-    <div className="loader">
-      <section aria-label="Skeleton loader" className="loader__inner">
-        <p aria-label="Skeleton loader text" className="loader__text">Skeleton loading text</p>
-        <p aria-label="Skeleton loader text" className="loader__text">More skeleton loading text</p>
-        <p aria-label="Skeleton loader text" className="loader__text">More skeleton loading text</p>
-        <p aria-label="Skeleton loader text" className="loader__text">Even more skeleton loading text</p>
+    <div className="loader-blog">
+      <section aria-label="Skeleton loader" className="loader-blog__inner">
+        <p aria-label="Skeleton loader text" className="loader-blog__text">Skeleton loading text</p>
+        <p aria-label="Skeleton loader text" className="loader-blog__text">More skeleton loading text</p>
+        <p aria-label="Skeleton loader text" className="loader-blog__text">More skeleton loading text</p>
+        <p aria-label="Skeleton loader text" className="loader-blog__text">Even more skeleton loading text</p>
       </section>
     </div>
   );
